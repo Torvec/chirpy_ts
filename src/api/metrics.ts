b1fce0d87ebb04a245f0e47e8e7b22a7a0166097
vonedward <me@edward-vonschondorf.dev>
2025-07-15 15:50:28 -0700

    Add ch.3 and most of ch.4 code

--- src/api/metrics.ts
+++ src/api/metrics.ts
@@ -1,11 +1,17 @@
 import type { Request, Response } from "express";
 import { config } from "../config.js";
 
-export async function handleMetrics(req: Request, res: Response) {
-  res.send(`Hits: ${config.fileserverHits}`);
+export async function handleMetrics(_: Request, res: Response) {
+  res.set({ "Content-Type": "text/html; charset=utf-8" });
+  res.send(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited ${config.fileserverHits} times!</p>
+  </body>
+</html>`);
 }
 
-export async function handleMetricsReset(req: Request, res: Response) {
+export async function handleMetricsReset(_: Request, res: Response) {
   config.fileserverHits = 0;
   res.write("Hits reset to 0");
   res.end();
--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,21 @@ import {
   middlewareLogResponses,
   middlewareMetricsInc,
 } from "./api/middleware.js";
+import { handleChirpsValidate } from "./api/chirps.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponses);
 
+app.use(express.json());
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
-app.get("/healthz", handleReadiness);
+app.get("/api/healthz", handleReadiness);
+app.get("/admin/metrics", handleMetrics);
+app.post("/admin/reset", handleMetricsReset);
 
-app.get("/metrics", handleMetrics);
-
-app.get("/reset", handleMetricsReset);
+app.post("/api/validate_chirp", handleChirpsValidate);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
