87828d49b8a1daa81ea87cfd375a5e1b9d670951
vonedward <me@edward-vonschondorf.dev>
2025-07-14 22:12:51 -0700

    Add ch.1 code

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+
+const app = express();
+const PORT = 8080;
+
+app.use("/app", express.static("./src/app"));
+
+app.get("/healthz", (req, res) => {
+  res.set({ "Content-Type": "text/plain; charset=utf-8" });
+  res.status(200);
+  res.send("OK");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
